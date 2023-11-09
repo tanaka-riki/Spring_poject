d10c553dfea741e461643ecafe99a6f2dbd7c486
DESKTOP-IFSUQTF\PC_User <tanaka2365r@gmail.com>
2023-11-10 02:39:47 +0900

    frst commit

--- src/main/java/com/example/demo/PracticeDoWhile.java
+++ src/main/java/com/example/demo/PracticeDoWhile.java
@@ -3,11 +3,11 @@ package com.example.demo;
 public class PracticeDoWhile {
 
 	public static void main(String[] args) {
-		int num = 100; // 初期値設定
+		int num = 100; 
 
 		do {
-			System.out.println(num); // 変数numを表示
-			num--; //１の減算
-		} while (num >= 0); // numが0以上の場合、ブロック内の処理が繰り返される
+			System.out.println(num); 
+			num--; 
+		} while (num >= 0); 
 	}
-} //100 から 1 ずつ減算して、0 まで表示するプログラム
+} 
