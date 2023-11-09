6b2d9c8056e4a86484a675e6a0704a8aba9620e6
DESKTOP-IFSUQTF\PC_User <tanaka2365r@gmail.com>
2023-11-10 02:45:42 +0900

    comitto

--- src/main/java/com/example/demo/PracticeWhile.java
+++ src/main/java/com/example/demo/PracticeWhile.java
@@ -3,8 +3,10 @@ package com.example.demo;
 public class PracticeWhile {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+		int num = 100; 
 
+		while (num >= 0) { 	
+			System.out.println(num); 
+		}
 	}
-
 }
